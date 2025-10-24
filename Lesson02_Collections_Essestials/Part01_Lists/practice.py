2d7936d41e26d693a10bc9ce8a0d41e8a0b92198
PyroCinAltar <lynnhilife@gmail.com>
2025-10-24 19:49:57 -0400

    Remembered to uncomment`

--- Lesson02_Collections_Essestials/Part01_Lists/practice.py
+++ Lesson02_Collections_Essestials/Part01_Lists/practice.py
@@ -1,32 +1,32 @@
 # Problem 1
-# def remove_duplicates(items):
-#     edit_list = []
-#     for item in items:
-#         if item not in edit_list:
-#             edit_list.append(item)
-#         else:
-#             continue
-#     return(edit_list)
+def remove_duplicates(items):
+    edit_list = []
+    for item in items:
+        if item not in edit_list:
+            edit_list.append(item)
+        else:
+            continue
+    return(edit_list)
 
-# print(remove_duplicates([1, 2, 2, 3, 1, 4]) )
-# print(remove_duplicates(["a","b","a","c"]))
-# print(remove_duplicates([5, 5, 5]))
-# print(remove_duplicates([]))
+print(remove_duplicates([1, 2, 2, 3, 1, 4]) )
+print(remove_duplicates(["a","b","a","c"]))
+print(remove_duplicates([5, 5, 5]))
+print(remove_duplicates([]))
 
 
 # Problem 2
-# def find_common(list1, list2):
-#     common = []
-#     for item in list1:
-#         if item in list2:
-#             common.append(item)
-#     return common
+def find_common(list1, list2):
+    common = []
+    for item in list1:
+        if item in list2:
+            common.append(item)
+    return common
 
 
-# print(find_common([1, 2, 3], [2, 3, 4]))
-# print(find_common(["a", "b"], ["c", "d"]))
-# print(find_common([1, 1, 2], [2, 2, 3]))
-# print(find_common([], [1, 2]))
+print(find_common([1, 2, 3], [2, 3, 4]))
+print(find_common(["a", "b"], ["c", "d"]))
+print(find_common([1, 1, 2], [2, 2, 3]))
+print(find_common([], [1, 2]))
 
 
 # # Problem 3
@@ -48,14 +48,14 @@ print(reverse_sublists([1, 2, 3], 5))
 
 
 # # Problem 4
-# def rotate_list(items, position):
-#     new_list = []
-#     for i in range(len(items)):
-#         rotate = (i - position) % len(items)
-#         new_list.append(items[rotate])
-#     return new_list
+def rotate_list(items, position):
+    new_list = []
+    for i in range(len(items)):
+        rotate = (i - position) % len(items)
+        new_list.append(items[rotate])
+    return new_list
 
-# print(rotate_list([1, 2, 3, 4, 5], 2))
-# print(rotate_list([1, 2, 3, 4, 5],-2))
-# print(rotate_list([1, 2, 3], 0))
-# print(rotate_list([1, 2, 3], 5))
+print(rotate_list([1, 2, 3, 4, 5], 2))
+print(rotate_list([1, 2, 3, 4, 5],-2))
+print(rotate_list([1, 2, 3], 0))
+print(rotate_list([1, 2, 3], 5))
