ad0d2697a24e1a8cc4ecbdda8dc40fd38ade3a21
PyroCinAltar <lynnhilife@gmail.com>
2025-11-12 09:49:33 -0500

    Assignemnt Finished

---
+++ Lesson02_Collections_Essestials/Part04_Review_Sets/set3.py
@@ -0,0 +1,35 @@
+# Q1
+'''
+wrong order and answer uh
+it prints [23] because everything except the first two values go into current and then append to streak.
+
+
+If programmed right, [8,8,7]
+'''
+
+
+# Q2
+'''
+NEXUS
+'''
+
+# Q3
+# def match_mvp(players):
+#     best_kd = 0.0
+#     best_player = ""
+#     for player, stats in players.items():
+#         kd_ratio = stats["kills"]/stats["deaths"]
+#         if kd_ratio > best_kd:
+#             best_kd = kd_ratio
+#             best_player = player
+            
+#     return best_player
+    
+
+# players_list = {
+#     "pheonix": {"kills": 28, "deaths": 12},
+#     "cipher": {"kills": 35, "deaths": 15},
+#     "blaze": {"kills": 22, "deaths": 18}
+# }
+
+# print(match_mvp(players_list))
