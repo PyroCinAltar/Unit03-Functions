aad9100754aedbe90c894a6156ca5cf3a3e111a2
PyroCinAltar <lynnhilife@gmail.com>
2025-12-01 08:45:44 -0500

    entry

---
+++ Lesson06_Entry_Point/math_ults.py
@@ -0,0 +1,7 @@
+def square(n):
+    return n * n
+
+print("testing1", square(2))
+
+if __name__ == "__main__":
+    print("Testing:", square(5))
---
+++ Lesson06_Entry_Point/test.py
@@ -0,0 +1,4 @@
+# import demo
+from math_ults import square
+
+print("Damage:", square(10))
