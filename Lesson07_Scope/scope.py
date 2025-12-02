a930499421a2e2e6654fbccd0a57b19f54741a7e
PyroCinAltar <lynnhilife@gmail.com>
2025-12-02 08:56:06 -0500

    SCope day 1

---
+++ Lesson07_Scope/scope.py
@@ -0,0 +1,12 @@
+# Scope - the visibility of variables, where it can be seen
+# Global - outside all functions
+# Local - inside a function(only visible there)
+
+# The Bug - Crashes
+# def add_bonus():
+#     score = score + 100
+    
+# score = 500
+# add_bonus()
+
+# solutions: pass and return, using the "global" keyword
